96bf6c41d2fe0edd674eb364aaad7213caa4947a
Sagnik Mitra <sagnik.mitra@juspay.in>
2024-12-18 11:44:05 +0530

    pre payment network tokenization

--- crates/api_models/src/admin.rs
+++ crates/api_models/src/admin.rs
@@ -1867,6 +1867,10 @@ pub struct ProfileCreate {
     #[schema(value_type = Option<Object>, example = r#"{ "click_to_pay": "mca_ushduqwhdohwd", "netcetera": "mca_kwqhudqwd" }"#)]
     pub authentication_product_ids:
         Option<common_types::payments::AuthenticationConnectorAccountMap>,
+
+    /// Indicates if network tokenization before first payment is enabled or not
+    #[serde(default)]
+    pub is_tokenize_before_payment_enabled: bool,
 }
 
 #[nutype::nutype(
@@ -1985,6 +1989,10 @@ pub struct ProfileCreate {
     #[schema(value_type = Option<Object>, example = r#"{ "click_to_pay": "mca_ushduqwhdohwd", "netcetera": "mca_kwqhudqwd" }"#)]
     pub authentication_product_ids:
         Option<common_types::payments::AuthenticationConnectorAccountMap>,
+
+    /// Indicates if network tokenization before first payment is enabled or not
+    #[serde(default)]
+    pub is_tokenize_before_payment_enabled: bool,
 }
 
 #[cfg(feature = "v1")]
@@ -2122,6 +2130,9 @@ pub struct ProfileResponse {
     #[schema(value_type = Option<Object>, example = r#"{ "click_to_pay": "mca_ushduqwhdohwd", "netcetera": "mca_kwqhudqwd" }"#)]
     pub authentication_product_ids:
         Option<common_types::payments::AuthenticationConnectorAccountMap>,
+
+    /// Indicates if network tokenization before first payment is enabled or not
+    pub is_tokenize_before_payment_enabled: bool,
 }
 
 #[cfg(feature = "v2")]
@@ -2246,6 +2257,9 @@ pub struct ProfileResponse {
     #[schema(value_type = Option<Object>, example = r#"{ "click_to_pay": "mca_ushduqwhdohwd", "netcetera": "mca_kwqhudqwd" }"#)]
     pub authentication_product_ids:
         Option<common_types::payments::AuthenticationConnectorAccountMap>,
+
+    /// Indicates if network tokenization before first payment is enabled or not
+    pub is_tokenize_before_payment_enabled: bool,
 }
 
 #[cfg(feature = "v1")]
@@ -2377,6 +2391,10 @@ pub struct ProfileUpdate {
     #[schema(value_type = Option<Object>, example = r#"{ "click_to_pay": "mca_ushduqwhdohwd", "netcetera": "mca_kwqhudqwd" }"#)]
     pub authentication_product_ids:
         Option<common_types::payments::AuthenticationConnectorAccountMap>,
+
+    /// Indicates if network tokenization before first payment is enabled or not
+    #[schema(default = false, example = false)]
+    pub is_tokenize_before_payment_enabled: Option<bool>,
 }
 
 #[cfg(feature = "v2")]
@@ -2489,6 +2507,10 @@ pub struct ProfileUpdate {
     #[schema(value_type = Option<Object>, example = r#"{ "click_to_pay": "mca_ushduqwhdohwd", "netcetera": "mca_kwqhudqwd" }"#)]
     pub authentication_product_ids:
         Option<common_types::payments::AuthenticationConnectorAccountMap>,
+
+    /// Indicates if network tokenization before first payment is enabled or not
+    #[schema(default = false, example = false)]
+    pub is_tokenize_before_payment_enabled: Option<bool>,
 }
 
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize, ToSchema)]
